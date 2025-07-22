6e36071c28d10297203cdfaa38aa4684d133543d
Mikael Guillin <mikael.guillin@protonmail.com>
2025-07-22 07:06:03 -0400

    Update map markers

--- src/Map.tsx
+++ src/Map.tsx
@@ -9,8 +9,28 @@ import { MapFiltersDialog } from "./MapFiltersDialog";
 
 export function Map() {
     const {open: filtersOpen, onOpen: onFiltersOpen, setOpen: setFiltersOpen} = useDisclosure();
-    const customIcon = new L.Icon({
-        iconUrl: 'https://images.unsplash.com/photo-1581486179957-f7c9fb3a0050?q=60&w=50&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    const icons = {
+        penguins: 'https://images.unsplash.com/photo-1581486179957-f7c9fb3a0050?q=60&w=50&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        giraffes: 'https://images.unsplash.com/photo-1547721064-da6cfb341d50?q=60&w=50&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        lions: 'https://images.unsplash.com/photo-1614027164847-1b28cfe1df60?q=60&w=50&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    }
+
+    const penguinsIcon = new L.Icon({
+        iconUrl: icons.penguins,
+        iconSize: [40, 40], // size of the icon
+        iconAnchor: [20, 32], // point of the icon which will correspond to marker's location
+        popupAnchor: [0, -32], // point from which the popup should open relative to the iconAnchor,
+        className: 'custom-marker',
+    });
+    const giraffesIcon = new L.Icon({
+        iconUrl: icons.giraffes,
+        iconSize: [40, 40], // size of the icon
+        iconAnchor: [20, 32], // point of the icon which will correspond to marker's location
+        popupAnchor: [0, -32], // point from which the popup should open relative to the iconAnchor,
+        className: 'custom-marker',
+    });
+    const lionsIcon = new L.Icon({
+        iconUrl: icons.lions,
         iconSize: [40, 40], // size of the icon
         iconAnchor: [20, 32], // point of the icon which will correspond to marker's location
         popupAnchor: [0, -32], // point from which the popup should open relative to the iconAnchor,
@@ -28,19 +48,19 @@ export function Map() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[40.8501455,-73.8792966]}>
+                <Marker position={[40.8501455,-73.8792966]} icon={giraffesIcon}>
                     <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
+                        Giraffes
                     </Popup>
                 </Marker>
-                <Marker position={[40.8511455,-73.8802966]}>
+                <Marker position={[40.8511455,-73.8802966]} icon={lionsIcon}>
                     <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
+                        Lions
                     </Popup>
                 </Marker>
-                <Marker position={[40.8511455,-73.8782966]} icon={customIcon}>
+                <Marker position={[40.8511455,-73.8782966]} icon={penguinsIcon}>
                     <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
+                        Penguins
                     </Popup>
                 </Marker>
                 <IconButton zIndex={999} position="absolute" left="10px" top="85px">
